3f9a1bb18f6174360fca419d4f099f192c3b7b9c
unasesoumu <liannaidanshenggu17@gmail.com>
2026-07-25 12:04:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,58 +3,55 @@ console.log("script読み込みOK");
 const searchInput = document.getElementById("searchInput");
 const results = document.getElementById("searchResults");
 
-const works = [
-    {
-        name: "「またのお越しはお待ちしておりません。」",
-        text: "CoC6版 ソロシナリオ 海底郵便局を舞台にした物語作品",
-        url: "work1.html"
-    }
-];
 
+if(searchInput && results){
 
-searchInput.addEventListener("input", function() {
+    searchInput.addEventListener("input", function() {
 
-    const keyword = this.value.toLowerCase();
+        const keyword = this.value.toLowerCase();
 
-    results.innerHTML = "";
+        results.innerHTML = "";
 
-    if (keyword === "") {
-        return;
-    }
-
-
-    works.forEach(work => {
-
-        if (
-            work.name.toLowerCase().includes(keyword) ||
-            work.text.toLowerCase().includes(keyword)
-        ) {
-
-            results.innerHTML += `
-
-            <div class="result-card">
-
-                <h3>
-                    ${work.name}
-                </h3>
-
-                <p>
-                    ${work.text}
-                </p>
-
-               <a href="${work.url}">
-    作品ページへ
-</a>
-
-            </div>
-
-            `;
-
+        if (keyword === "") {
+            return;
         }
+
+
+        works.forEach(work => {
+
+            if (
+                work.name.toLowerCase().includes(keyword) ||
+                work.text.toLowerCase().includes(keyword)
+            ) {
+
+                results.innerHTML += `
+
+                <div class="result-card">
+
+                    <h3>
+                        ${work.name}
+                    </h3>
+
+                    <p>
+                        ${work.text}
+                    </p>
+
+                    <a href="${work.url}">
+                        作品ページへ
+                    </a>
+
+                </div>
+
+                `;
+
+            }
+
+        });
 
     });
 
-});
+}
+
 const menuButton = document.getElementById("menuButton");
 const menuPanel = document.getElementById("menuPanel");
 
